a60c872363d997ac26d07491fbeabffaa2f19312
Mike <ruecktmi@gmail.com>
2019-07-11 17:52:16 -0700

    end video 88

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -31,6 +31,12 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was succesffuly deleted"
+    redirect_to articles_path
+  end
 
   private
 
