aaee848364130608e236eea370d24912e4325acb
Mike <ruecktmi@gmail.com>
2019-07-11 17:37:46 -0700

    end video 82

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
-
+  def index
+    @articles = Article.all
+  end
   def new
-
-  @article = Article.new
+    @article = Article.new
+  end
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -15,7 +19,16 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+        flash[:notice] = "Article was successfully updated"
+        redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  def show
     @article = Article.find(params[:id])
   end
 
