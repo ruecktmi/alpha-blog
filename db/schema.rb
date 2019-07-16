afb934a17fd4e76d0a0ac6be81e4eb2633a7d064
Mike <ruecktmi@gmail.com>
2019-07-15 19:15:05 -0700

    add association and debugger view

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_004527) do
+ActiveRecord::Schema.define(version: 2019_07_16_011608) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
