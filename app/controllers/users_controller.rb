82191ebf01d0a089d46d84d23ed7b6267b3802d6
Mike <ruecktmi@gmail.com>
2019-07-22 11:09:05 -0700

    sign in aftersign up new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Alpha Blog"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -57,4 +58,5 @@ class UsersController < ApplicationController
       flash[:danger] = "You can only eidt your account"
       redirect_to root_path
   end
+end
 end
