b10177a4e756fc12a5d37f6fe6fc97beefeff57e
Mike <ruecktmi@gmail.com>
2019-07-22 17:13:16 -0700

    update categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -13,6 +13,17 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_match "sports", response.body
   end
 
+  test "Invalid category submittion results in failure" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: {name:""} }
+    end
+    assert_template 'categories/new'
+  end
+
+
+
 
 
 end
